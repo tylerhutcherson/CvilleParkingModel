968d6234e2146db698711c96b6b2c4728b090ccb
tylerhutcherson <tyler.hutcherson@metismachine.com>
2019-03-02 21:35:10 -0500

    get the dang spacing right

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-import os
 import json
 from time import time
 from queue import Empty
@@ -10,9 +9,8 @@ from messaging.skafos_queue import SkafosQueue
 import model.schema as s
 from model import features
 
-
-
 def _evaluate_parking_model(msg, model):
+    # Map input lat lon to a block id
     res = features.closest_block(msg)
     msg['block_id'] = res['block_id']
 
@@ -32,39 +30,39 @@ def _publish_response(response, queue):
     )
 
 def consume_input_queue(skafos, queue, model):
-  try:
-    while True:
-      # Retrieve a message from input queue
-      method, properties, body = queue.consume(
-        queue_name=s.INPUT_QUEUE_NAME
-      )
-      # Start the timer
-      start_time = time()
-      # Load message and make a prediction
-      msg = json.loads(body)
-      response = _evaluate_parking_model(msg, model)
-      # Publish response to the results queue
-      _publish_response(response, queue)
-      queue.ack(method.delivery_tag)
-      # Deliver performance metrics back to user
-      runtime = time() - start_time
-      skafos.log(f'Prediction delivered in {runtime} seconds', labels=['report'])
-  except Empty:
-    skafos.log("Queue Empty! Packing up and going home.", labels=['predict'])
-  except Exception as e:
-    skafos.log(f'Error while scoring: {e}', labels=['error', 'predict'])
-    raise e
-  skafos.queue.stop()
+    try:
+        while True:
+            # Retrieve a message from input queue
+            method, properties, body = queue.consume(
+              queue_name=s.INPUT_QUEUE_NAME
+            )
+            # Start the timer
+            start_time = time()
+            # Load message and make a prediction
+            msg = json.loads(body)
+            response = _evaluate_parking_model(msg, model)
+            # Publish response to the results queue
+            _publish_response(response, queue)
+            queue.ack(method.delivery_tag)
+            # Deliver performance metrics back to user
+            runtime = time() - start_time
+            skafos.log(f'Prediction delivered in {runtime} seconds', labels=['report'])
+    except Empty:
+        skafos.log("Queue Empty! Packing up and going home.", labels=['predict'])
+    except Exception as e:
+        skafos.log(f'Error while scoring: {e}', labels=['error', 'predict'])
+        raise e
+    skafos.queue.stop()
 
 
 if __name__ == "__main__":
-  # Initialize Skafos and Queue
-  ska = Skafos()
-  queue = SkafosQueue()
+    # Initialize Skafos and Queue
+    ska = Skafos()
+    queue = SkafosQueue()
 
-  # Load parking model - update soon
-  parking_model = None
+    # Load parking model - update soon
+    parking_model = None
 
-  # Consume from input queue
-  consume_input_queue(skafos=ska, queue=queue, model=parking_model)
-  ska.log("Closing application. Goodbye.", labels=['shutdown'])
+    # Consume from input queue
+    consume_input_queue(skafos=ska, queue=queue, model=parking_model)
+    ska.log("Closing application. Goodbye.", labels=['shutdown'])
